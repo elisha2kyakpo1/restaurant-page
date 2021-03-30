6b2db88c20f0c0fbceae6977b34ef86fde562547
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-03-30 13:49:29 +0200

    Add linter files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+    const list = document.querySelector('#content');
+    const nav = document.createElement('ul');
+  
+    nav.innerHTML = `
+      <li><a href="#" class="heading">About</a></li>
+      <li><a href="#" class="heading"></a>Today's menu</li>
+      <li><a href="#" class="heading"></a>Contact us</li>
+    `;
+    const url = list.appendChild(nav);
+    console.log(url);
